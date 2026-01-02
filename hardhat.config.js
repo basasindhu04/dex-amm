4e864d123ca1098443c2da42c474b785156d36c3
basasindhu04 <basasindhu04@gmail.com>
2026-01-02 22:23:27 +0530

    Simplify hardhat.config.js formatting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
 
 module.exports = {
   solidity: {
     version: "0.8.19",
     settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
+      optimizer: { enabled: true, runs: 200 }
     }
   },
   networks: {
-    hardhat: {
-      chainId: 31337
-    }
+    hardhat: { chainId: 31337 }
   }
 };
