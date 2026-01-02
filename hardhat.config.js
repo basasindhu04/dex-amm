5979d39004b0ab2dd5d1b3e6e3d92357e105f7df
basasindhu04 <basasindhu04@gmail.com>
2026-01-02 22:24:16 +0530

    Format hardhat.config.js for better readability

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,15 @@ module.exports = {
   solidity: {
     version: "0.8.19",
     settings: {
-      optimizer: { enabled: true, runs: 200 }
-    }
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
   networks: {
-    hardhat: { chainId: 31337 }
-  }
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
